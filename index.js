4803d47b3abc24daf3fbd1b89da918056c331b0b
NpatsL <napatsorn.la@ku.th>
2024-04-07 22:16:08 +0700

    feat: Add file analysis endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 require('dotenv').config()
 
 var app = express();
@@ -11,10 +13,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
